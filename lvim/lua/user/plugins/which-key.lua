1f94db0cac1d8bbc1875f2c3ae9cb1942e392448
Eric Eldredge <lettertwo@gmail.com>
2022-04-29 19:44:46 -0400

    Add_Trouble_config (#5)

--- lvim/lua/user/plugins/which-key.lua
+++ lvim/lua/user/plugins/which-key.lua
@@ -11,16 +11,6 @@ function WhichKey.config()
 
 	-- Use which-key to add extra bindings with the leader-key prefix
 	lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-	lvim.builtin.which_key.mappings["t"] = {
-		name = "+Trouble",
-		r = { "<cmd>Trouble lsp_references<cr>", "References" },
-		f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-		d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-		q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-		l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-		w = { "<cmd>Trouble workspace_diagnostics<cr>", "Diagnostics" },
-		t = { "<cmd>Trouble telescope<cr>", "Telescope" },
-	}
 	lvim.builtin.which_key.mappings["<cr>"] = { "<cmd>update!<CR>", "Save, if changed" }
 	lvim.builtin.which_key.mappings["b"] = vim.tbl_deep_extend("error", lvim.builtin.which_key.mappings["b"], {
 		w = { "<cmd>w<CR>", "Write current buffer" },
