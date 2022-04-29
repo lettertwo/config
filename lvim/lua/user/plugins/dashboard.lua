b70d69fcce8ebd9f041c8d5b7bd35c3ef41a23a0
Eric Eldredge <lettertwo@gmail.com>
2022-04-29 12:06:17 -0400

    Fix deprecated dashboard config

--- lvim/lua/user/plugins/dashboard.lua
+++ lvim/lua/user/plugins/dashboard.lua
@@ -1,26 +1,11 @@
 local Dashboard = {}
 
 Dashboard.config = function()
-	if not lvim.builtin.dashboard.active then
+	if not lvim.builtin.alpha.active then
 		return
 	end
 
-	-- Set session directory
-	vim.g.dashboard_session_directory = vim.fn.expand("$HOME/.cache/lvim/sessions")
-
-	-- Dashboard
-	local ds = lvim.builtin.dashboard.custom_section
-	-- local project_desc = ds.c.description
-	-- local project_cmd = ds.c.command
-	ds.b = {
-		description = ds.d.description,
-		command = ds.d.command,
-	}
-	ds.c = {
-		description = { "  Git files          " },
-		command = "Telescope git_files",
-	}
-	ds.d = nil
+	lvim.builtin.alpha.mode = "dashboard"
 end
 
 return Dashboard
