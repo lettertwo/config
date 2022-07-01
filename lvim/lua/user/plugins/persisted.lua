339d5d8b4d0b319e26dd0ecf536e625de281e0aa
Eric Eldredge <lettertwo@gmail.com>
2022-07-01 11:14:22 -0400

    Improve session switching behavior

--- lvim/lua/user/plugins/persisted.lua
+++ lvim/lua/user/plugins/persisted.lua
@@ -8,6 +8,20 @@ function Persisted.config()
       "~/.config",
       "~/Code",
     },
+    after_source = function()
+      -- Reload the LSP servers
+      vim.lsp.stop_client(vim.lsp.get_active_clients())
+    end,
+    telescope = {
+      before_source = function()
+        -- Close all open buffers
+        pcall(vim.cmd, "bufdo bwipeout")
+      end,
+      after_source = function(session)
+        -- Change the git branch
+        pcall(vim.cmd, "git checkout " .. session.branch)
+      end,
+    },
   })
 
   local _, telescope = pcall(require, "telescope")
