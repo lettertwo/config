cc81d3e84c206ff3b668bed8f6c6980162c5d659
Eric Eldredge <lettertwo@gmail.com>
2022-07-01 11:08:00 -0400

    Add function to cd to git root

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -83,6 +83,11 @@ function zsh_stats() {
   fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
 
+# cd to git root directory.
+function cdr() {
+  cd "$(git rev-parse --show-toplevel)"
+}
+
 #
 # vim aliases
 #
