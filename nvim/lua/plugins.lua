088588687cd13a245c2decbf119d39aa45f6c855
Eric Eldredge <lettertwo@gmail.com>
2022-07-08 00:57:33 -0400

    Add nvim-web-devicons

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,11 +12,16 @@ packer.init({
 local use = packer.use
 packer.reset()
 
-use "wbthomason/packer.nvim"    -- Packer can manage itself
-use 'lewis6991/impatient.nvim'  -- Gotta go fast
-use "nvim-lua/plenary.nvim"     -- A common dependency in lua plugins
-use "folke/which-key.nvim"      -- Show keymap help
+-- "always on" plugins; no setup necessary! --
+use "wbthomason/packer.nvim"       -- Packer can manage itself
+use 'lewis6991/impatient.nvim'     -- Gotta go fast
+use "nvim-lua/plenary.nvim"        -- A common dependency in lua plugins
+use "kyazdani42/nvim-web-devicons" -- Icons used by lots of other nvim plugins
 
+-- some assembly required --
+use "folke/which-key.nvim"         -- Show keymap help
+
+-- Colorscheme --
 use { "~/.local/share/laserwave", requires = { "rktjmp/lush.nvim", "rktjmp/shipwright.nvim" } }
 
 packer.compile() -- since we didn't use packer.startup(), manually compile plugins
