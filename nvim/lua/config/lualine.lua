2bde50ba48e8d52952cabf396cd66b76bf5da869
Eric Eldredge <lettertwo@gmail.com>
2022-07-09 13:41:36 -0400

    Add lualine

---
+++ nvim/lua/config/lualine.lua
@@ -0,0 +1,145 @@
+local colors = {
+  bg = "#202328",
+  fg = "#bbc2cf",
+  yellow = "#ECBE7B",
+  cyan = "#008080",
+  darkblue = "#081633",
+  green = "#98be65",
+  orange = "#FF8800",
+  violet = "#a9a1e1",
+  magenta = "#c678dd",
+  purple = "#c678dd",
+  blue = "#51afef",
+  red = "#ec5f67",
+}
+
+local window_width_limit = 70
+
+local function hide_in_width()
+  return vim.fn.winwidth(0) > window_width_limit
+end
+
+local branch = {
+  -- "b:gitsigns_head",
+  "branch",
+  icon = " ",
+  color = { gui = "bold" },
+  cond = hide_in_width,
+}
+
+local filename = { "filename", color = {}, cond = nil }
+
+local filetype = { "filetype", cond = hide_in_width }
+
+local diff = {
+  "diff",
+  source = function()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+      return {
+        added = gitsigns.added,
+        modified = gitsigns.changed,
+        removed = gitsigns.removed,
+      }
+    end
+  end,
+  symbols = { added = "  ", modified = " ", removed = " " },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.yellow },
+    removed = { fg = colors.red },
+  },
+  cond = nil,
+}
+
+local persisting = {
+  function()
+    if vim.g.persisting then
+      return " "
+    else
+      return " "
+    end
+  end,
+  color = { fg = colors.bg },
+  cond = hide_in_width,
+}
+
+local treesitter = {
+  function()
+    local b = vim.api.nvim_get_current_buf()
+    if next(vim.treesitter.highlighter.active[b]) then
+      return ""
+    end
+    return ""
+  end,
+  color = { fg = colors.bg },
+  cond = hide_in_width,
+}
+
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
+  cond = hide_in_width,
+}
+
+-- Adapted from https://github.com/LunarVim/LunarVim/blob/48320e/lua/lvim/core/lualine/components.lua#L82
+local lsp = {
+  function()
+    local buf_clients = vim.lsp.buf_get_clients()
+    local buf_ft = vim.bo.filetype
+    local buf_client_names = {}
+
+    -- add client
+    for _, client in pairs(buf_clients) do
+      if client.name ~= "null-ls" then
+        table.insert(buf_client_names, client.name)
+      end
+    end
+
+    -- add formatter
+    local formatters = require "lvim.lsp.null-ls.formatters"
+    local supported_formatters = formatters.list_registered(buf_ft)
+    vim.list_extend(buf_client_names, supported_formatters)
+
+    -- add linter
+    local linters = require "lvim.lsp.null-ls.linters"
+    local supported_linters = linters.list_registered(buf_ft)
+    vim.list_extend(buf_client_names, supported_linters)
+
+    local unique_client_names = vim.fn.uniq(buf_client_names)
+    return "[" .. table.concat(unique_client_names, ", ") .. "]"
+  end,
+  cond = function() 
+    return hide_in_width() or vim.lsp.buf_get_clients() == nil
+  end,
+}
+
+require("lualine").setup {
+  options = {
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "NvimTree", "Outline", "netrw" },
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { branch, filename },
+    lualine_c = {
+      diff,
+    },
+    lualine_x = { diagnostics, lsp, filetype },
+    lualine_y = {},
+    lualine_z = { treesitter, persisting },
+  },
+  inactive_sections = {
+    lualine_a = {
+      "filename",
+    },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {},
+  },
+}
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -19,7 +19,10 @@ use "nvim-lua/plenary.nvim"        -- A common dependency in lua plugins
 use "kyazdani42/nvim-web-devicons" -- Icons used by lots of other nvim plugins
 
 -- some assembly required --
-use "folke/which-key.nvim"         -- Show keymap help
+use "folke/which-key.nvim"         -- See keymap.lua
+
+-- Status bar --
+use { "nvim-lualine/lualine.nvim", config = [[ require("config.lualine") ]] }
 
 -- Colorscheme --
 use { "~/.local/share/laserwave", requires = { "rktjmp/lush.nvim", "rktjmp/shipwright.nvim" } }
