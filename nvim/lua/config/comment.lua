3c7381cdda4494052ef08948de293685e9a344b4
Eric Eldredge <lettertwo@gmail.com>
2022-08-29 13:29:45 -0400

    Update treesitter comment integration

--- nvim/lua/config/comment.lua
+++ nvim/lua/config/comment.lua
@@ -11,27 +11,8 @@ require("Comment").setup({
     ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
     extended = false,
   },
-  -- From https://github.com/numToStr/Comment.nvim#-hooks
-  -- and https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
-  pre_hook = function(ctx)
-    local U = require("Comment.utils")
-
-    -- Determine whether to use linewise or blockwise commentstring
-    local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
-
-    -- Determine the location where to calculate commentstring from
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring({
-      key = type,
-      location = location,
-    })
-  end,
+  -- From https://github.com/JoosepAlviste/nvim-ts-context-commentstring#commentnvim
+  pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
 })
 
 local keymap = require("keymap")
