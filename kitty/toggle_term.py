a2cde1c002690649f7c895f86bf8094d1a79e0eb
Eric Eldredge <lettertwo@gmail.com>
2022-09-04 01:33:06 -0400

    Add toggle-term-like kitty window behavior

---
+++ kitty/toggle_term.py
@@ -0,0 +1,22 @@
+def main():
+    pass
+
+def handle_result(args, __, ___, boss):
+    new = True if "new" in args else False
+    cwd = True if "cwd" in args else False
+    tab = boss.active_tab
+    if tab is not None:
+        new = new or len(tab) == 1
+        if new:
+            if cwd:
+                tab.new_window(cwd=tab.get_cwd_of_active_window())
+            else:
+                tab.new_window()
+        if tab.current_layout.name == 'stack':
+            tab.last_used_layout()
+            tab.windows.make_previous_group_active()
+        elif not new:
+            tab.first_window()
+            tab.goto_layout('stack')
+
+handle_result.no_ui = True
