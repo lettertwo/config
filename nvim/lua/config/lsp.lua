37d01ab601e8d6536dce29d45c864526b6b1fef6
Eric Eldredge <lettertwo@gmail.com>
2022-10-29 13:35:46 -0400

    Let clippy yell at me

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -141,12 +141,22 @@ end
 -- Rust tools setup
 rt.setup({
   server = {
-    on_attach = function(_, bufnr)
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
       -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
     end,
+    checkOnSave = {
+      allFeatures = true,
+      overrideCommand = {
+        "cargo",
+        "clippy",
+        "--workspace",
+        "--message-format=json",
+        "--all-targets",
+        "--all-features",
+      },
+    },
   },
 })
 
