ebfb332425d3cf2b9975673f28f83f5bb507d649
Eric Eldredge <lettertwo@gmail.com>
2022-11-14 01:00:07 -0500

    Fix hover styling with noice

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -159,11 +159,3 @@ rt.setup({
     },
   },
 })
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "rounded",
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = "rounded",
-})
--- nvim/lua/config/noice.lua
+++ nvim/lua/config/noice.lua
@@ -1,7 +1,16 @@
 require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
   presets = {
-    long_message_to_split = true,
-    command_palette = true,
+    long_message_to_split = true, -- long messages will be sent to a split
+    command_palette = true, -- position the cmdline and popupmenu together
+    lsp_doc_border = true, -- add a border to hover docs and signature help
   },
   routes = {
     {
