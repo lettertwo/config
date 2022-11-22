46410826a61f4ead131d867700c230d82ea028fa
Eric Eldredge <lettertwo@gmail.com>
2022-11-21 23:55:23 -0500

    Detect flow lib filetype

--- nvim/filetype.lua
+++ nvim/filetype.lua
@@ -1,9 +1,11 @@
 local function is_flowtype()
+  ---@diagnostic disable-next-line: undefined-field
   return vim.fn.getline(1):match("//%s*@flow")
 end
 
 vim.filetype.add({
   extension = {
+    flow = "flowtype",
     js = function()
       if is_flowtype() then
         return "flowtype"
