70be52094854362da8f05e33ef546103b36933f0
Eric Eldredge <lettertwo@gmail.com>
2023-02-03 17:15:18 -0500

    Clean up which-key group labels and ui filetypes

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -30,18 +30,7 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
-  -- TODO: use the global special filetype list here
-  pattern = {
-    "qf",
-    "help",
-    "man",
-    "notify",
-    "lspinfo",
-    "spectre_panel",
-    "startuptime",
-    "tsplayground",
-    "PlenaryTestPopup",
-  },
+  pattern = require("config").filetypes.ui,
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
