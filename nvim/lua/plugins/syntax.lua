12dbebd12780643d1a4a0bb9f0a3083684c593e0
Eric Eldredge <lettertwo@gmail.com>
2023-02-10 13:29:49 -0500

    Add treesitter playground

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -64,6 +64,7 @@ return {
     dependencies = {
       "RRethy/nvim-treesitter-endwise",
       "JoosepAlviste/nvim-ts-context-commentstring",
+      "nvim-treesitter/playground",
     },
     keys = {
       { "<c-space>", desc = "Increment selection" },
@@ -117,6 +118,24 @@ return {
           node_incremental = "<C-space>",
           scope_incremental = "<nop>",
           node_decremental = "<bs>",
+        },
+      },
+      playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+          toggle_query_editor = "o",
+          toggle_hl_groups = "i",
+          toggle_injected_languages = "t",
+          toggle_anonymous_nodes = "a",
+          toggle_language_display = "I",
+          focus_language = "f",
+          unfocus_language = "F",
+          update = "R",
+          goto_node = "<cr>",
+          show_help = "?",
         },
       },
     },
