e1fe2e94c5bab6fb5e9cd4d188c908fa9e468bcc
Matthew Dapena-Tretter <m@tthewwithanm.com>
2023-02-16 23:00:17 -0800

    Enable formatting on save for ESLint
    This change will run "EslintFixAll" to format JS files on save.

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -64,7 +64,14 @@ return {
             },
           },
         },
-        "eslint",
+        eslint = {
+          on_attach = function(client, bufnr)
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
+          end,
+        },
         "html",
         "cssls",
         "pyright",
