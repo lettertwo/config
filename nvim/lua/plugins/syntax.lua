bf47f73ed26af9fa3a293ef414be14f81806550c
Eric Eldredge <lettertwo@gmail.com>
2023-03-06 12:50:14 -0500

    Add which-key descriptions for text objects

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -194,6 +194,7 @@ return {
   -- better text-objects
   {
     "echasnovski/mini.ai",
+    event = "VeryLazy",
     keys = {
       { "a", mode = { "x", "o" } },
       { "i", mode = { "x", "o" } },
@@ -224,6 +225,50 @@ return {
     config = function(_, opts)
       local ai = require("mini.ai")
       ai.setup(opts)
+      -- from https://github.com/LazyVim/LazyVim/blob/b1b5b46/lua/lazyvim/plugins/coding.lua#L181m
+      -- register all text objects with which-key
+      if require("util").has("which-key.nvim") then
+        ---@type table<string, string|table>
+        local i = {
+          [" "] = "Whitespace",
+          ['"'] = 'Balanced "',
+          ["'"] = "Balanced '",
+          ["`"] = "Balanced `",
+          ["("] = "Balanced (",
+          [")"] = "Balanced ) including white-space",
+          [">"] = "Balanced > including white-space",
+          ["<lt>"] = "Balanced <",
+          ["]"] = "Balanced ] including white-space",
+          ["["] = "Balanced [",
+          ["}"] = "Balanced } including white-space",
+          ["{"] = "Balanced {",
+          ["?"] = "User Prompt",
+          _ = "Underscore",
+          a = "Argument",
+          b = "Balanced ), ], }",
+          c = "Class",
+          f = "Function",
+          o = "Block, conditional, loop",
+          q = "Quote `, \", '",
+          t = "Tag",
+        }
+        local a = vim.deepcopy(i)
+        for k, v in pairs(a) do
+          a[k] = v:gsub(" including.*", "")
+        end
+
+        local ic = vim.deepcopy(i)
+        local ac = vim.deepcopy(a)
+        for key, name in pairs({ n = "Next", l = "Last" }) do
+          i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+          a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+        end
+        require("which-key").register({
+          mode = { "o", "x" },
+          i = i,
+          a = a,
+        })
+      end
     end,
   },
 }
