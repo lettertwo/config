e722b2ea1e05ebc7f8685e94e0a4d75da4a2ec8c
Eric Eldredge <lettertwo@gmail.com>
2023-04-03 13:27:29 -0400

    [nvim] add symbols outline

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -33,6 +33,17 @@ return {
     },
   },
 
+  --symbols outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = { "SymbolsOutline", "SymbolsOutlineOpen" },
+    event = "VeryLazy",
+    keys = {
+      { "<leader>S", "<cmd>SymbolsOutline<cr>", desc = "Symbols outline" },
+    },
+    opts = {},
+  },
+
   -- search/replace in multiple files
   {
     "windwp/nvim-spectre",
@@ -108,6 +119,7 @@ return {
         ["<leader>f"] = { name = "+file/find" },
         ["<leader>g"] = { name = "+git" },
         ["<leader>l"] = { name = "+lsp" },
+        ["<leader>n"] = { name = "+neorg" },
         ["<leader>q"] = { name = "+quit/session" },
         ["<leader>s"] = { name = "+search" },
         ["<leader>se"] = { name = "+emoji" },
@@ -126,5 +138,20 @@ return {
       { "<leader>bd", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer" },
       { "<leader>bD", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
+  },
+
+  -- sticky buffers
+  {
+    "stevearc/stickybuf.nvim",
+    event = "VeryLazy",
+    cmd = { "PinBuffer", "PinBuftype", "PinFiletype", "Unpin" },
+    keys = {
+      { "<leader>bpb", "<cmd>PinBuffer<cr>", desc = "Pin buffer" },
+      { "<leader>bpt", "<cmd>PinBuftype<cr>", desc = "Pin buffer type" },
+      { "<leader>bpf", "<cmd>PinFiletype<cr>", desc = "Pin fileytype" },
+      { "<leader>bpu", "<cmd>Unpin<cr>", desc = "Unpin buffer" },
+    },
+    -- TODO: configure sticky buffers for symbols outline, neotree, toggleterm, trouble, etc.
+    opts = {},
   },
 }
