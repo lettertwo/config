2086942af639de462c5d62a6890668204d3cb062
Eric Eldredge <lettertwo@gmail.com>
2023-04-03 17:39:09 -0400

    [kitty] lint

--- kitty/toggle_term.py
+++ kitty/toggle_term.py
@@ -1,7 +1,8 @@
 def main():
     pass
 
-def handle_result(args, __, ___, boss):
+
+def handle_result(args, __, ___, boss):  # pyright: ignore
     new = True if "new" in args else False
     cwd = True if "cwd" in args else False
     tab = boss.active_tab
@@ -12,11 +13,12 @@ def handle_result(args, __, ___, boss):
                 tab.new_window(cwd=tab.get_cwd_of_active_window())
             else:
                 tab.new_window()
-        if tab.current_layout.name == 'stack':
+        if tab.current_layout.name == "stack":
             tab.last_used_layout()
             tab.windows.make_previous_group_active()
         elif not new:
             tab.first_window()
-            tab.goto_layout('stack')
+            tab.goto_layout("stack")
+
 
 handle_result.no_ui = True
