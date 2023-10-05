0625e632cd6bec740b8add70de7cd7d8c69fa5ee
Eric Eldredge <lettertwo@gmail.com>
2023-10-05 14:45:35 -0400

    [nvim] workaround telescope pickers exiting in insert mode

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -253,6 +253,15 @@ return {
       --     search_dirs = { vim.env.XDG_CONFIG_HOME },
       --   })
       -- end
+
+      -- Workaround for https://github.com/nvim-telescope/telescope.nvim/issues/2501
+      vim.api.nvim_create_autocmd("WinLeave", {
+        callback = function()
+          if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+          end
+        end,
+      })
     end,
   },
 }
