e9fb8735b91dde3f519c840af217d86d5276fceb
Eric Eldredge <lettertwo@gmail.com>
2023-10-06 12:27:25 -0400

    [nvim] Fix Telescope insert mode workaround

--- nvim/lua/plugins/telescope/init.lua
+++ nvim/lua/plugins/telescope/init.lua
@@ -258,7 +258,11 @@ return {
       vim.api.nvim_create_autocmd("WinLeave", {
         callback = function()
           if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+            vim.schedule(function()
+              if vim.fn.mode() == "i" then
+                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-c>", true, false, true), "i", false)
+              end
+            end)
           end
         end,
       })
