b5e6f917c73fcf07b6d3c1341feed5a3163d4b59
Eric Eldredge <lettertwo@gmail.com>
2024-04-01 18:58:47 -0400

    [nvim] fix closing of invalid floats

--- nvim/lua/util/init.lua
+++ nvim/lua/util/init.lua
@@ -34,7 +34,7 @@ end
 
 function ConfigUtil.close_floats()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
-    if vim.api.nvim_win_get_config(win).relative ~= "" then
+    if vim.api.nvim_win_is_valid(win) and vim.api.nvim_win_get_config(win).relative ~= "" then
       vim.api.nvim_win_close(win, false)
     end
   end
