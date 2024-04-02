520b6462e17bf403c704637fac81ddc74de603b9
Eric Eldredge <lettertwo@gmail.com>
2024-04-02 16:24:04 -0400

    [nvim] add satellite.nvim

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -228,4 +228,64 @@ return {
       top = {},
     },
   },
+  {
+    "lewis6991/satellite.nvim",
+    event = "VeryLazy",
+    cmd = { "SatelliteDisable", "SatelliteEnable", "SatelliteRefresh" },
+    opts = {
+      current_only = false,
+      winblend = 50,
+      excluded_filetypes = filetypes.ui,
+      handlers = {
+        cursor = {
+          enable = true,
+          -- Supports any number of symbols
+          symbols = { "⎺", "⎻", "⎼", "⎽" },
+          -- symbols = { '⎻', '⎼' }
+          -- Highlights:
+          -- - SatelliteCursor (default links to NonText
+        },
+        search = {
+          enable = true,
+          -- Highlights:
+          -- - SatelliteSearch (default links to Search)
+          -- - SatelliteSearchCurrent (default links to SearchCurrent)
+        },
+        diagnostic = {
+          enable = true,
+          signs = { "-", "=", "≡" },
+          min_severity = vim.diagnostic.severity.HINT,
+          -- Highlights:
+          -- - SatelliteDiagnosticError (default links to DiagnosticError)
+          -- - SatelliteDiagnosticWarn (default links to DiagnosticWarn)
+          -- - SatelliteDiagnosticInfo (default links to DiagnosticInfo)
+          -- - SatelliteDiagnosticHint (default links to DiagnosticHint)
+        },
+        gitsigns = {
+          enable = true,
+          signs = { -- can only be a single character (multibyte is okay)
+            add = "│",
+            change = "│",
+            delete = "-",
+          },
+          -- Highlights:
+          -- SatelliteGitSignsAdd (default links to GitSignsAdd)
+          -- SatelliteGitSignsChange (default links to GitSignsChange)
+          -- SatelliteGitSignsDelete (default links to GitSignsDelete)
+        },
+        marks = {
+          enable = true,
+          show_builtins = false, -- shows the builtin marks like [ ] < >
+          key = "m",
+          -- Highlights:
+          -- SatelliteMark (default links to Normal)
+        },
+        quickfix = {
+          signs = { "-", "=", "≡" },
+          -- Highlights:
+          -- SatelliteQuickfix (default links to WarningMsg)
+        },
+      },
+    },
+  },
 }
