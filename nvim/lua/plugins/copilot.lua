db783453d4d749bbbe88ec3a8c54ac160665a6dd
Eric Eldredge <lettertwo@gmail.com>
2024-04-02 16:21:20 -0400

    [nvim] update CopilotChat and config

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -48,12 +48,38 @@ return {
       --   row = 1,
       -- },
       mappings = {
-        close = "q",
-        reset = "<C-l>",
-        complete = "<Tab>",
-        submit_prompt = "<CR>",
-        accept_diff = "<C-y>",
-        show_diff = "<C-d>",
+        complete = {
+          detail = "Use @<Tab> or /<Tab> for options.",
+          insert = "<Tab>",
+        },
+        close = {
+          normal = "q",
+          insert = "<C-c>",
+        },
+        reset = {
+          normal = "<C-l>",
+          insert = "<C-l>",
+        },
+        submit_prompt = {
+          normal = "<CR>",
+          insert = "<C-m>",
+        },
+        accept_diff = {
+          normal = "<C-y>",
+          insert = "<C-y>",
+        },
+        yank_diff = {
+          normal = "gy",
+        },
+        show_diff = {
+          normal = "gd",
+        },
+        show_system_prompt = {
+          normal = "gp",
+        },
+        show_user_selection = {
+          normal = "gs",
+        },
       },
     },
     keys = {
