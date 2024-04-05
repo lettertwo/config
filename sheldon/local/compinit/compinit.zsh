42d0ef3b52355562a68e8a43f45777c134c1a8eb
Eric Eldredge <lettertwo@gmail.com>
2024-04-05 14:15:16 -0400

    [zsh] fix zoxide compinit race

--- sheldon/local/compinit/compinit.zsh
+++ sheldon/local/compinit/compinit.zsh
@@ -7,6 +7,13 @@ comprebuild() {
   compinit -d $XDG_CACHE_HOME/zsh/zcompdump
 }
 
+complist() {
+  for command completion in ${(kv)_comps:#-*(-|-,*)}
+    do
+      printf "%-32s %s\n" $command $completion
+  done | sort
+}
+
 # Rebuild comp dump whenever it is more than 24 hrs old.
 # based on https://gist.github.com/ctechols/ca1035271ad134841284
 for dump in $XDG_CACHE_HOME/zsh/zcompdump(N.mh+24); do
