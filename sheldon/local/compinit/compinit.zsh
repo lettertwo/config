489159329e9120892b2673a86b8450e684e84e82
Eric Eldredge <lettertwo@gmail.com>
2024-04-05 18:42:29 -0400

    [zsh] improve automatic comprebuild

--- sheldon/local/compinit/compinit.zsh
+++ sheldon/local/compinit/compinit.zsh
@@ -3,21 +3,56 @@
 autoload -Uz compinit
 
 comprebuild() {
-  rm -f $XDG_CACHE_HOME/zsh/zcompdump 2>/dev/null
-  compinit -d $XDG_CACHE_HOME/zsh/zcompdump
+  local zcompdump=${1:-$XDG_CACHE_HOME/zsh/zcompdump}
+  rm -f $1:-$zcompdump 2>/dev/null
+  compinit -d $zcompdump
 }
 
 complist() {
-  for command completion in ${(kv)_comps:#-*(-|-,*)}
+  for command completion in ${(kv)_comps}
     do
       printf "%-32s %s\n" $command $completion
   done | sort
 }
 
-# Rebuild comp dump whenever it is more than 24 hrs old.
-# based on https://gist.github.com/ctechols/ca1035271ad134841284
-for dump in $XDG_CACHE_HOME/zsh/zcompdump(N.mh+24); do
-  comprebuild
-done
+function() {
+  local zcompdump=${1:-$XDG_CACHE_HOME/zsh/zcompdump}
 
-compinit -C -d $XDG_CACHE_HOME/zsh/zcompdump
+  if [[ ! -f $zcompdump ]]; then
+    echo "rebuilding early"
+    comprebuild $zcompdump
+    return
+  fi
+
+  local rebuild=false
+  local zcompdump_mtime=$(date -u -r $(stat -f %m $zcompdump) "+%Y-%m-%dT%H:%M:%SZ")
+
+  if command -v fd >/dev/null 2>&1; then
+    # Rebuild compdump whenever any files in $fpath are newer
+    for dir in $fpath; do
+      if [[ ! -d $dir ]]; then
+        continue
+      fi
+
+      local newer_files=$(fd . $dir --type f --newer $zcompdump_mtime)
+      if [[ -n $newer_files ]]; then
+        rebuild=true
+        break
+      fi
+    done
+  fi
+
+  # Rebuild compdump whenever it is more than 24 hrs old.
+  # based on https://gist.github.com/ctechols/ca1035271ad134841284
+  for dump in $zcompdump(N.mh+24); do
+    rebuild=true
+    break
+  done
+
+  if $rebuild; then
+    echo "rebuilding"
+    comprebuild $zcompdump
+  else
+    compinit -C -d $zcompdump
+  fi
+}
