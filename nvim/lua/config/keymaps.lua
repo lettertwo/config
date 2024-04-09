87660df752ba444e5a25847859ef16a6e386b1c0
Eric Eldredge <lettertwo@gmail.com>
2024-04-09 17:02:53 -0400

    [nvim] switch from navigator to smart-splits

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,12 +14,6 @@ vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set("n", "<C-d>", "'<C-d>zz'", { expr = true, silent = true, desc = "Scroll down" })
 vim.keymap.set("n", "<C-u>", "'<C-u>zz'", { expr = true, silent = true, desc = "Scroll up" })
 
--- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
 -- Move Lines
 vim.keymap.set("n", "<A-j>", ":m .+1<cr>==", { desc = "Move down", silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down", silent = true })
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -22,16 +22,43 @@ return {
   },
 
   {
-    "craigmac/Navigator.nvim",
-    cmd = { "NavigatorLeft", "NavigatorRight", "NavigatorUp", "NavigatorDown", "NavigatorPrevious" },
-    keys = {
-      { "<c-h>", "<cmd>NavigatorLeft<cr>", desc = "NavigatorLeft" },
-      { "<c-l>", "<cmd>NavigatorRight<cr>", desc = "NavigatorRight" },
-      { "<c-k>", "<cmd>NavigatorUp<cr>", desc = "NavigatorUp" },
-      { "<c-j>", "<cmd>NavigatorDown<cr>", desc = "NavigatorDown" },
-      -- { "<CTRL-p>", "<cmd>NavigatorPrevious</cr>" },
+    "mrjones2014/smart-splits.nvim",
+    event = "VeryLazy",
+    build = "./kitty/install-kittens.bash",
+    opts = {
+      at_edge = "stop", -- 'wrap' | 'split' | 'stop'
     },
-    opts = {},
+    -- stylua: ignore start
+    keys = {
+      -- resizing splits
+      { "<C-S-h>", function() require("smart-splits").resize_left() end, desc = "Resize window left" },
+      { "<C-S-l>", function() require("smart-splits").resize_right() end, desc = "Resize window right" },
+      { "<C-S-j>", function() require("smart-splits").resize_down() end, desc = "Resize window down" },
+      { "<C-S-k>", function() require("smart-splits").resize_up() end, desc = "Resize window up" },
+      -- moving between splits
+      { "<C-h>", function() require("smart-splits").move_cursor_left() end, desc = "Go to the left window" },
+      { "<C-j>", function() require("smart-splits").move_cursor_down() end, desc = "Go to the down window"},
+      { "<C-k>", function() require("smart-splits").move_cursor_up() end, desc = "Go to the up window" },
+      { "<C-l>", function() require("smart-splits").move_cursor_right() end, desc = "Go to the right window" },
+      -- swapping buffers between windows
+      { "<C-w>xh", function() require("smart-splits").swap_buf_left() end, desc = "swap left" },
+      { "<C-w>xj", function() require("smart-splits").swap_buf_down() end, desc = "swap down" },
+      { "<C-w>xk", function() require("smart-splits").swap_buf_up() end, desc = "swap up" },
+      { "<C-w>xl", function() require("smart-splits").swap_buf_right() end, desc = "swap right" },
+      { "<C-w>R", function() require("smart-splits").start_resize_mode() end, desc = "Enter window resize mode" },
+
+
+      { "<C-w>H", "<C-S-h>", remap = true, desc = "Resize window left" },
+      { "<C-w>L", "<C-S-l>", remap = true, desc = "Resize window right" },
+      { "<C-w>J", "<C-S-j>", remap = true, desc = "Resize window down" },
+      { "<C-w>K", "<C-S-k>", remap = true, desc = "Resize window up" },
+      { "<C-w>h", "<C-h>", remap = true, desc = "Go to the left window" },
+      { "<C-w>j", "<C-j>", remap = true, desc = "Go to the down window"},
+      { "<C-w>k", "<C-k>", remap = true, desc = "Go to the up window" },
+      { "<C-w>l", "<C-l>", remap = true, desc = "Go to the right window" },
+      { "<C-w>xx", "<C-w><C-x>", remap = true, desc = "swap current with next" },
+    },
+    -- stylua: ignore end
   },
 
   -- A common dependency in lua plugins. Also useful for testing plugins.
