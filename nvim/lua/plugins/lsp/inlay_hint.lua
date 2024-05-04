4eb18c38a4221d04fe19b2bf6fa27e688054ce88
Eric Eldredge <lettertwo@gmail.com>
2024-05-04 19:50:51 -0400

    [nvim] fix inlay_hint toggling

--- nvim/lua/plugins/lsp/inlay_hint.lua
+++ nvim/lua/plugins/lsp/inlay_hint.lua
@@ -6,10 +6,10 @@ function M.on_attach(client, buffer)
   local inlay_hint = vim.lsp.buf.inlay_hint or vim.lsp.inlay_hint
 
   if inlay_hint ~= nil and client.supports_method("textDocument/inlayHint") then
-    inlay_hint.enable(buffer, true)
+    inlay_hint.enable(true, { bufnr = buffer })
 
     local toggle_inlay_hint = Util.create_toggle("inlay_hints", "b", function(enabled)
-      inlay_hint.enable(buffer, enabled)
+      inlay_hint.enable(enabled, { bufnr = buffer })
     end)
 
     require("plugins.lsp.keymaps").apply({ buffer = buffer, client = client }, {
