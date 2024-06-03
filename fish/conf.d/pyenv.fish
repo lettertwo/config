d6929733b9522f877b14fb2c346b4c05fe380973
Eric Eldredge <lettertwo@gmail.com>
2024-06-03 15:23:06 -0400

    [fish] fix pyenv root

--- fish/conf.d/pyenv.fish
+++ fish/conf.d/pyenv.fish
@@ -1,4 +1,4 @@
 type -q pyenv || return 1
-set -q PYENV_ROOT; or set -Ux PYENV_ROOT $HOME/.pyenv
+set -q PYENV_ROOT; or set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv
 cachecmd pyenv init - | source
 fish_add_path $PYENV_ROOT/bin
