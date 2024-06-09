12b85874d7093636f3b0b37d54e558b40161b878
Eric Eldredge <lettertwo@gmail.com>
2024-06-09 12:26:24 -0400

    [nvim] catch more cases in TODO highlighting

--- nvim/lua/plugins/syntax.lua
+++ nvim/lua/plugins/syntax.lua
@@ -94,20 +94,23 @@ return {
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = "BufReadPost",
     opts = {
-      -- highlighting for rust todo/unimplemented macros:
-      -- From https://github.com/folke/todo-comments.nvim/issues/186#issuecomment-1592342384
       keywords = {
+        -- highlighting for rust todo/unimplemented macros
+        -- From https://github.com/folke/todo-comments.nvim/issues/186#issuecomment-1592342384
         TODO = { alt = { "todo", "unimplemented" } },
+        -- highlighting for alternative takes on hacky comments
+        HACK = { alt = { "hacky", "Hacky" } },
       },
       highlight = {
         pattern = {
-          [[.*<(KEYWORDS)\s*:]],
+          [[.*<(KEYWORDS).*:]],
+          -- pattern to match rust todo/unimplemented macros
           [[.*<(KEYWORDS)\s*!\(]],
         },
         comments_only = false,
       },
       search = {
-        pattern = [[\b(KEYWORDS)(:|!\()]],
+        pattern = [[\b(KEYWORDS)(.*:|\s*!\()]],
       },
     },
     -- stylua: ignore
