d8f9ece107207ed490a08348593c6ee7ac494b11
Eric Eldredge <lettertwo@gmail.com>
2024-11-04 18:36:23 -0500

    [fish] cache pyenv cmd rehash

--- fish/conf.d/pyenv.fish
+++ fish/conf.d/pyenv.fish
@@ -1,4 +1,12 @@
 type -q pyenv || return 1
 set -q PYENV_ROOT; or set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv
-cachecmd pyenv init - | source
 fish_add_path $PYENV_ROOT/bin
+
+# Inlined from:
+# cachecmd -- pyenv init - --no-rehash --no-push-path | source
+if not contains -- "/Users/eeldredge/.local/share/pyenv/shims" $PATH
+  set -gx PATH '/Users/eeldredge/.local/share/pyenv/shims' $PATH
+end
+set -gx PYENV_SHELL fish
+
+cachecmd pyenv rehash
