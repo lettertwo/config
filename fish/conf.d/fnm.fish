e5dcae0ef852fa7c076cb034f9b01385391f6f61
Eric Eldredge <lettertwo@gmail.com>
2024-11-16 12:59:58 -0500

    [fish] fix fnm multishell

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,2 +1,6 @@
 type -q fnm || return 1
-cachecmd -- fnm env --use-on-cd | source
+
+# NOTE: Cannot cache this command because it generates version-specific symlinks
+# which, when cached, will inadvertently make node version changes universal
+# to all shells instead of local to the current shell.
+fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled --resolve-engines | source
