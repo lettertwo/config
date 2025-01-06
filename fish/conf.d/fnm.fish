6c7dd9999630798a43dc4089b02feed14fa91c92
Eric Eldredge <lettertwo@gmail.com>
2025-01-06 18:27:53 -0500

    [fish] move fnm install dir to XDG_DATA_HOME

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -1,5 +1,7 @@
 type -q fnm || return 1
 
+set -q FNM_DIR; or set -Ux FNM_DIR $XDG_DATA_HOME/fnm
+
 # NOTE: Cannot cache this command because it generates version-specific symlinks
 # which, when cached, will inadvertently make node version changes universal
 # to all shells instead of local to the current shell.
