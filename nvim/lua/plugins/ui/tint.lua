a661e5959962f298115fb7710828782c54501d05
Eric Eldredge <lettertwo@gmail.com>
2025-03-15 13:51:04 -0400

    [nvim] improve tint behavior on higher contrast colorschemees

--- nvim/lua/plugins/ui/tint.lua
+++ nvim/lua/plugins/ui/tint.lua
@@ -3,10 +3,30 @@ return {
   {
     "levouh/tint.nvim",
     event = "VeryLazy",
-    opts = {
-      tint = -50,
-      saturation = 0.5,
-      highlight_ignore_patterns = { "WinSeparator", "Status.*", "IndentBlankline*" },
-    },
+    opts = function()
+      local transforms = {
+        require("tint.transforms").tint(-45),
+        require("tint.transforms").saturate(0.5),
+      }
+
+      local function update_transforms()
+        local hex = "#C1C1C"
+        local bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+        if bg then
+          hex = "#" .. require("tint.colors").get_hex(bg)
+        else
+        end
+        transforms[1] = require("tint.transforms").tint_with_threshold(-50, hex, 50)
+      end
+
+      vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+        callback = update_transforms,
+        group = vim.api.nvim_create_augroup("tint-color-scheme-change", { clear = true }),
+      })
+
+      update_transforms()
+
+      return { transforms = transforms }
+    end,
   },
 }
