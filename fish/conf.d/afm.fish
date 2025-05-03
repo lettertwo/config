85342f14068a55608cc9d3735551350f327331c1
Eric Eldredge <lettertwo@gmail.com>
2025-05-03 15:57:25 -0400

    [fish] add afm path

--- fish/conf.d/afm.fish
+++ fish/conf.d/afm.fish
@@ -1,3 +1,9 @@
 if test -e $HOME/.afm-git-configrc
     source $HOME/.afm-git-configrc
 end
+
+# Add afm-tools to PATH
+if test -e $AFM_HOME/afm-tools/path
+    # Add bin directories to path.
+    fish_add_path --global --prepend $AFM_HOME/afm-tools/path
+end
