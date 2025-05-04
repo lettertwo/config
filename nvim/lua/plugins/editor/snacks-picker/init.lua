4e1a6839f409b430aa4cb5d800751cc072cfa202
Eric Eldredge <lettertwo@gmail.com>
2025-05-04 15:09:34 -0400

    [nvim] adjust switch picker bindings

--- nvim/lua/plugins/editor/snacks-picker/init.lua
+++ nvim/lua/plugins/editor/snacks-picker/init.lua
@@ -134,9 +134,9 @@ return {
 
     --stylua: ignore
   keys = {
-      { "<leader><space>", LazyVim.pick("switch", { scope = "root" }), desc = "Switch (root)" },
-      { "<leader>r", LazyVim.pick("switch"), desc = "Switch" },
-      { "<leader>R", LazyVim.pick("switch", { scope = "workspace" }), desc = "Switch (workspace)" },
+      { "<leader><space>", LazyVim.pick("switch", { scope = "workspace" }), desc = "Switch (workspace)" },
+      { "<leader>r", LazyVim.pick("switch", { scope = "root" }), desc = "Switch (root)" },
+      { "<leader>R", LazyVim.pick("switch"), desc = "Switch (global)" },
       { "<leader>'", LazyVim.pick("grapple"), desc = "Grapple" },
       { "<leader>nn", function() Snacks.picker.notifications() end, desc = "Notification History" },
       { "<leader>qp", LazyVim.pick("projects"), desc = "Projects" },
