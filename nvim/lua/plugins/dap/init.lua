2c854b22824c3c992f2fe2107ae92e412bcdf777
Eric Eldredge <lettertwo@gmail.com>
2025-07-11 14:35:47 -0400

    [nvim] reset dap config; add dap config for lua

--- nvim/lua/plugins/dap/init.lua
+++ nvim/lua/plugins/dap/init.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = {
+        "codelldb",
+        "js-debug-adapter",
+        "node-debug2-adapter",
+      },
+    },
+  },
+}
--- nvim/lua/plugins/lang/lua.lua
+++ nvim/lua/plugins/lang/lua.lua
@@ -11,6 +11,7 @@ return {
       })
     end,
   },
+
   {
     "nvim-neotest/neotest",
     opts = {
@@ -19,6 +20,62 @@ return {
       },
     },
   },
+
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = {
+        "local-lua-debugger-vscode",
+      },
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    opts = function(_, opts)
+      local dap = require("dap")
+
+      dap.adapters["local-lua"] = {
+        type = "executable",
+        command = "node",
+        args = {
+          vim.fn.expand("$MASON/share/local-lua-debugger-vscode/extension/debugAdapter.js"),
+        },
+      }
+
+      dap.configurations.lua = {
+        {
+          name = "Current file (local-lua-dbg, lua)",
+          type = "local-lua",
+          repl_lang = "lua",
+          request = "launch",
+          cwd = "${workspaceFolder}",
+          program = {
+            lua = "luajit",
+            file = "${file}",
+          },
+          verbose = true,
+          extensionPath = vim.fn.expand("$MASON/share/local-lua-debugger-vscode/"),
+          args = {},
+        },
+        {
+          name = "Current file (local-lua-dbg, neovim lua interpreter with nlua)",
+          type = "local-lua",
+          repl_lang = "lua",
+          request = "launch",
+          cwd = "${workspaceFolder}",
+          program = {
+            lua = "nlua",
+            file = "${file}",
+          },
+          verbose = true,
+          extensionPath = vim.fn.expand("$MASON/share/local-lua-debugger-vscode/"),
+          args = {},
+        },
+      }
+    end,
+  },
+
   {
     "folke/which-key.nvim",
     ft = "lua",
