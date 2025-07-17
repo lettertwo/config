d05b74e1244d04656af5982090a6382b0e1b431c
Eric Eldredge <erice@Erics-MBP-2.lan>
2025-07-17 14:22:08 -0400

    [nvim] expand capabilities of title_path string util

--- nvim/lua/util/string.lua
+++ nvim/lua/util/string.lua
@@ -45,8 +45,16 @@ function StringUtil.smart_shorten_path(path, opts)
   return path
 end
 
+---@class TitlePathOpts
+---@field cwd string?
+---@field target_width number?
+---@field ambiguous_filetypes string[]?
+---@field ambiguous_segments string[]?
+---@field nextjs_segment_patterns string[]?
+
+---@type TitlePathOpts
 local DEFAULT_TITLE_PATH_OPTS = {
-  ambiguous_segments = {
+  ambiguous_filetypes = {
     "init.lua",
     "index.js",
     "index.ts",
@@ -56,34 +64,98 @@ local DEFAULT_TITLE_PATH_OPTS = {
     "init.rs",
     "lib.rs",
     "main.rs",
+    "README.md",
+  },
+
+  ambiguous_segments = {
     "src",
+    "test",
+    "tests",
+    "__tests__",
+  },
+
+  nextjs_segment_patterns = {
+    -- Nested files
+    "/?app/.*/page%.([jt]sx?)$",
+    "/?app/.*/default%.([jt]sx?)$",
+    "/?app/.*/forbidden%.([jt]sx?)$",
+    "/?app/.*/not%-found%.([jt]sx?)$",
+    "/?app/.*/unauthorized%.([jt]sx?)$",
+    "/?app/.*/layout%.([jt]sx?)$",
+    "/?app/.*/loading%.([jt]sx?)$",
+    "/?app/.*/error%.([jt]sx?)$",
+    "/?app/.*/template%.([jt]sx?)$",
+    "/?app/.*/route%.([jt]sx?)$",
+
+    -- MDX files
+    "/?app/.*/page%.mdx?$",
+    "/?app/.*/default%.mdx?$",
+    "/?app/.*/forbidden%.mdx?$",
+    "/?app/.*/not%-found%.mdx?$",
+    "/?app/.*/unauthorized%.mdx?$",
+    "/?app/.*/loading%.mdx?$",
+    "/?app/.*/error%.mdx?$",
   },
 }
 
 StringUtil.SEP = package.config:sub(1, 1)
 
+---param path string
+---param patterns? string[] List of Lua patterns to match against the path. Defaults to
+---the patterns defined in DEFAULT_TITLE_PATH_OPTS.nextjs_segment_patterns.
+function StringUtil.matches_any_pattern(path, patterns)
+  patterns = patterns or DEFAULT_TITLE_PATH_OPTS.nextjs_segment_patterns
+  for _, pat in ipairs(patterns) do
+    if path:match(pat) then
+      return true
+    end
+  end
+  return false
+end
+
 --- @param path string?
---- @param opts? { cwd: string?, target_width: number?, ambiguous_segments: string[]? }
+--- @param opts? TitlePathOpts Options for title path to be merged with default options.
 function StringUtil.title_path(path, opts)
   opts = vim.tbl_deep_extend("keep", opts or {}, DEFAULT_TITLE_PATH_OPTS)
   path = StringUtil.smart_shorten_path(path, { cwd = opts.cwd, target_width = opts.target_width })
 
   local segments = vim.split(path, StringUtil.SEP)
+  local titlepath = {}
 
-  local title_path = {}
-
-  local i = #segments
-
-  while i > 0 do
-    local segment = segments[i]
-    table.insert(title_path, 1, segment)
-    if not vim.list_contains(opts.ambiguous_segments, segment) then
-      break
+  -- Next.js segment pattern disambiguation
+  if StringUtil.matches_any_pattern(path, opts.nextjs_segment_patterns) then
+    for i = #segments, 1, -1 do
+      if segments[i] == "app" then
+        for j = i + 1, #segments do
+          table.insert(titlepath, segments[j])
+        end
+        break
+      end
     end
-    i = i - 1
+  else
+    local filename = segments[#segments]
+    segments[#segments] = nil -- Remove the filename from segments
+
+    for i = #segments, 1, -1 do
+      if i > 1 and vim.list_contains(opts.ambiguous_segments, segments[i]) then
+        for j = i - 1, #segments do
+          table.insert(titlepath, segments[j])
+        end
+        table.insert(titlepath, filename)
+        break
+      end
+    end
+
+    if #titlepath == 0 then
+      if #segments and vim.list_contains(opts.ambiguous_filetypes, filename) then
+        titlepath = { segments[#segments], filename }
+      else
+        titlepath = { filename }
+      end
+    end
   end
 
-  return table.concat(title_path, StringUtil.SEP)
+  return table.concat(titlepath, StringUtil.SEP)
 end
 
 function StringUtil.timeago(time)
