caf216c920a9694477fdde17418d632859e35286
Eric Eldredge <lettertwo@gmail.com>
2025-07-18 18:16:47 -0400

    [nvim] fix lang-specific dap configs

--- nvim/lua/plugins/lang/rust.lua
+++ nvim/lua/plugins/lang/rust.lua
@@ -1,13 +1,12 @@
 return {
   {
     "nvim-neotest/neotest",
+    dependencies = {
+      "mrcjkb/rustaceanvim",
+    },
+    optional = true,
     opts = function(_, opts)
-      local local_opts = {
-        adapters = {
-          require("rustaceanvim.neotest"),
-        },
-      }
-      return vim.tbl_deep_extend("force", opts or {}, local_opts)
+      vim.list_extend(opts.adapters, { "rustaceanvim.neotest" })
     end,
   },
 }
