233ce0eaf087a05eccd667cdca87734df2fb72f8
Eric Eldredge <lettertwo@gmail.com>
2025-07-21 14:03:02 -0400

    [kitty] link scripts from nvim rather than copying

--- nvim/lua/modes/pager.lua
+++ nvim/lua/modes/pager.lua
@@ -1,13 +1,23 @@
 return {
   {
     "mikesmithgh/kitty-scrollback.nvim",
-    lazy = true,
-    cond = vim.g.pager,
-    cmd = {
-      "KittyScrollbackGenerateKittens",
-      "KittyScrollbackCheckHealth",
-      "KittyScrollbackGenerateCommandLineEditing",
-    },
+    ---@module "lazy"
+    ---@param plugin LazyPlugin
+    build = function(plugin)
+      local function resolve(filepath)
+        return vim.fs.normalize(vim.fs.joinpath(plugin.dir, filepath))
+      end
+
+      local function link(filepath)
+        local filename = vim.fs.basename(filepath)
+        return vim.fn.system(string.format("ln -sf %s $XDG_CONFIG_HOME/kitty/%s", resolve(filepath), filename))
+      end
+
+      link("python/kitty_scrollback_nvim.py")
+      link("python/kitty_scroll_prompt.py")
+      link("python/loading.py")
+    end,
+    cmd = { "KittyScrollbackCheckHealth" },
     event = { "User KittyScrollbackLaunch" },
     opts = {
       {
