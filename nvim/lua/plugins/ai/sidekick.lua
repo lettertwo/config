2ae59814243ec6035092767d0910da11d6422757
Eric Eldredge <erice@Erics-MBP-2.lan>
2025-10-03 13:55:14 -0400

    [nvim] claude-specific sidekick improvements

--- nvim/lua/plugins/ai/sidekick.lua
+++ nvim/lua/plugins/ai/sidekick.lua
@@ -1,11 +1,68 @@
+-- @module "sidekick"
+
+---@param keys string
+---@param mode? string default is 'mx'. See :h feedkeys()
+local function feedkeys(keys, mode)
+  keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
+  vim.api.nvim_feedkeys(keys, mode or "mx", false)
+end
+
+-- Key mappings for Claude in sidekick.nvim terminal buffers.
+---@type table<string, sidekick.cli.Keymap|false>
+local claude_keys = {
+  new_line = {
+    "<s-cr>",
+    function(t)
+      t:send("\\")
+      t:submit()
+    end,
+    mode = "t",
+  },
+  start_insert = {
+    "i",
+    function(t)
+      if vim.fn.search([[-- INSERT]], "nw") == 0 then
+        t:send(vim.keycode("i"))
+      end
+      vim.cmd("startinsert")
+    end,
+    mode = "n",
+  },
+  term_normal = {
+    "<Esc>",
+    function(t)
+      -- Just a couple of dumb heuristics to determine if claude is looking for <esc>.
+      -- It's quite possible that we don't cover all scenarios here.
+      if vim.fn.search([[-- INSERT\|Esc to\|Esc/Tab to]], "nw") ~= 0 then
+        t:send(vim.keycode("<Esc>"))
+      else
+        vim.cmd("stopinsert")
+      end
+    end,
+    mode = "t",
+  },
+}
+
 return {
   {
     "folke/sidekick.nvim",
+    keys = {
+      { "<leader>ac", "<cmd>Sidekick cli show name=claude_continue<cr>", desc = "Claude Continue" },
+      { "<leader>ar", "<cmd>Sidekick cli show name=claude_resume<cr>", desc = "Claude Resume" },
+      { "<leader>af", "<cmd>Sidekick cli show name=claude_fork<cr>", desc = "Claude Fork" },
+    },
     ---@module "sidekick"
     ---@type sidekick.Config
     opts = {
       cli = {
+        tools = {
+          claude = { cmd = { "claude", "--verbose" }, keys = claude_keys },
+          claude_continue = { cmd = { "claude", "--verbose", "--continue" }, keys = claude_keys },
+          claude_resume = { cmd = { "claude", "--verbose", "--resume" }, keys = claude_keys },
+          claude_fork = { cmd = { "claude", "--verbose", "--resume", "--fork-session" }, keys = claude_keys },
+        },
         win = {
+          wo = { winbar = "%!v:lua.require'edgy.window'.edgy_winbar()" },
           -- stylua: ignore
           keys = {
             hide_n = false,
@@ -13,12 +70,43 @@ return {
             win_p = false,
             blur = false,
             hide = { "<c-q>", mode = { "t", "n" } },
-            move_left = { "<c-h>", function() vim.cmd.wincmd("h"); vim.cmd.stopinsert() end },
-            move_down = { "<c-j>", function() vim.cmd.wincmd("j"); vim.cmd.stopinsert() end },
-            move_up = { "<c-k>", function() vim.cmd.wincmd("k"); vim.cmd.stopinsert() end },
-            move_right = { "<c-l>", function() vim.cmd.wincmd("l"); vim.cmd.stopinsert() end },
-            new_line = { "<s-cr>", function(t) t:send("\\" ); t:submit() end, mode = "t" },
+            move_l = { "<c-h>", function() feedkeys("<c-h>") end },
+            move_d = { "<c-j>", function() feedkeys("<c-j>") end },
+            move_u = { "<c-k>", function() feedkeys("<c-k>") end },
+            move_r = { "<c-l>", function() feedkeys("<c-l>") end },
           },
+        },
+      },
+    },
+  },
+  {
+    "folke/edgy.nvim",
+    opts = {
+      right = {
+        {
+          ft = "sidekick_terminal",
+          size = { width = 0.4 },
+          title = function()
+            local title = "Sidekick"
+            local name = vim.b.sidekick_cli
+            local term_title = vim.b.term_title
+            if type(name) == "string" then
+              if name:find("claude") == 1 then
+                name = "Claude"
+              else
+                name = name:sub(1, 1):upper() .. name:sub(2)
+              end
+              title = name
+            end
+            if type(term_title) == "string" then
+              local segments = vim.split(vim.b.term_title, "//")
+              if #segments >= 2 then
+                term_title = segments[2]
+              end
+              title = title .. " (" .. term_title .. ")"
+            end
+            return title
+          end,
         },
       },
     },
