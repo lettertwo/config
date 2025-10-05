be55710ae2077c0c6fe05df9b1f590d14032fde4
Eric Eldredge <lettertwo@gmail.com>
2025-10-05 12:13:50 -0400

    [nvim] use which-key defaults

--- nvim/lua/plugins/editor/which-key.lua
+++ nvim/lua/plugins/editor/which-key.lua
@@ -1,21 +1,8 @@
 return {
   {
     "folke/which-key.nvim",
-    opts = function(_, opts)
-      opts.preset = "modern"
-      opts.disable = { ft = LazyVim.config.filetypes.ui }
-      opts.delay = function(ctx)
-        return ctx.mode == "x" and 500 or 0
-      end
-
-      -- Disable expands for groups in the default spec, e.g.,
-      -- numeric mappings for open buffers and windows.
-      opts.spec[1] = vim.tbl_map(function(item)
-        item.expand = nil
-        return item
-      end, opts.spec[1])
-
-      return opts
-    end,
+    opts = {
+      delay = 0,
+    },
   },
 }
