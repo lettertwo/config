073fe2915f941516f2a6d7518bb21cd23eb7a3f2
Eric Eldredge <lettertwo@gmail.com>
2025-10-20 17:07:30 -0400

    [task] remove

--- Brewfile
+++ Brewfile
@@ -47,10 +47,6 @@ brew "graphviz"
 brew "hyperfine"
 brew "jq"
 
-# taskwarrior
-brew "task"
-brew "tasksh"
-
 # neovim dependencies
 # See https://github.com/neovim/neovim/wiki/Building-Neovim#macos--homebrew
 brew "ninja"
--- task/hooks/on-launch.clean-expired-daily-tasks.lua
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env -S LUA_PATH=${XDG_CONFIG_HOME}/task/?.lua\_${XDG_CONFIG_HOME}/task/lua
-
-local client = require("task_client"):new()
-local uuids = client:fetch_expired_daily_tasks({ uuids = true })
-if #uuids > 0 then
-	io.stderr:write(string.format("Found %d expired daily tasks\n", #uuids))
-	client:delete(unpack(uuids))
-end
