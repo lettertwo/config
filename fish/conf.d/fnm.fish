4ea03a711d4a9a3b038b447a028c0c0369d56fc6
Test User <test@example.com>
2026-01-08 14:26:39 -0500

    [fish] re-enable fnm

--- fish/conf.d/fnm.fish
+++ fish/conf.d/fnm.fish
@@ -6,4 +6,4 @@ set -q FNM_DIR; or set -Ux FNM_DIR $XDG_DATA_HOME/fnm
 # NOTE: Cannot cache this command because it generates version-specific symlinks
 # which, when cached, will inadvertently make node version changes universal
 # to all shells instead of local to the current shell.
-# fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled --resolve-engines | source
+fnm env --use-on-cd --version-file-strategy recursive --corepack-enabled --resolve-engines | source
