0918d33ed3aee8bb7267c4f64e5eecd09f6c707f
Eric Eldredge <lettertwo@gmail.com>
2026-01-27 15:35:48 -0500

    [nvim] remove local plugin dir
    we can use the pattern established in laserwave and occurrence plugin
    modules instead

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -14,7 +14,6 @@ require("lazy").setup({
     { import = "plugins.lang" },
     { import = "plugins.ai" },
     { import = "modes" },
-    { import = "local" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
