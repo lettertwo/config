eb63a876344e2f708812637757a492175f187b83
Eric Eldredge <lettertwo@gmail.com>
2026-01-27 16:02:04 -0500

    [nvim] use snacks statuscolumn

--- nvim/lua/plugins/ui/statuscol.lua
+++ nvim/lua/plugins/ui/statuscol.lua
@@ -1,32 +1,14 @@
 return {
-  { "folke/snacks.nvim", optional = true, opts = { statuscolumn = { enabled = false } } },
   {
-    "luukvbaal/statuscol.nvim",
-    event = "VeryLazy",
-    opts = function()
-      local builtin = require("statuscol.builtin")
-      return {
-        separator = " ",
-        relculright = true,
-        setopt = true,
-        ft_ignore = LazyVim.config.filetypes.ui,
-        segments = {
-          -- sign
-          { text = { "%s" }, click = "v:lua.ScSa" },
-          -- line number
-          {
-            text = { builtin.lnumfunc, " " },
-            condition = { true, builtin.not_empty },
-            click = "v:lua.ScLa",
-          },
-          -- fold
-          {
-            text = { builtin.foldfunc, " " },
-            condition = { true, builtin.not_empty },
-            click = "v:lua.ScFa",
-          },
-        },
-      }
-    end,
+    "folke/snacks.nvim",
+    optional = true,
+    opts = {
+      ---@module "snacks"
+      ---@type snacks.statuscolumn.Config
+      statuscolumn = {
+        left = { "sign", "mark" }, -- priority of signs on the left (high to low)
+        right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      },
+    },
   },
 }
