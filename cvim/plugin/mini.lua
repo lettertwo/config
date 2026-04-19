4fe7bed62ad24056fdd949434151618c0f8df1ed
Eric Eldredge <lettertwo@gmail.com>
2026-04-19 17:21:02 -0400

    [cvim] add mini

---
+++ cvim/plugin/mini.lua
@@ -0,0 +1,22 @@
+local map = vim.keymap.set
+
+Config.add("nvim-mini/mini.nvim")
+
+Config.once("BufReadPost", function()
+	require("mini.trailspace").setup()
+	require("mini.operators").setup()
+	require("mini.bracketed").setup()
+
+	require("mini.align").setup({
+		-- Module mappings. Use `''` (empty string) to disable one.
+		mappings = {
+			start = "",
+			start_with_preview = "ga",
+		},
+		modifiers = {
+			["1"] = function(steps)
+				table.insert(steps.pre_justify, require("mini.align").gen_step.filter("n == 1"))
+			end,
+		},
+	})
+end)
