980b8cd10a69cb8f2570dfbb4daf8045c97f1986
Eric Eldredge <lettertwo@gmail.com>
2026-04-19 17:26:55 -0400

    [cvim] add snacks

---
+++ cvim/plugin/snacks.lua
@@ -0,0 +1,54 @@
+Config.add("folke/snacks.nvim")
+
+local opts = {
+	notifier = { level = vim.log.levels.INFO },
+	image = {},
+	indent = {
+		filter = function(buf)
+			return vim.g.snacks_indent ~= false
+				and vim.b[buf].snacks_indent ~= false
+				and vim.bo[buf].buftype == ""
+				and not vim.list_contains(Config.filetypes.ui, vim.bo[buf].filetype)
+		end,
+		animate = { enabled = false },
+		indent = { char = "│" },
+		scope = {
+			enabled = true,
+			only_current = true,
+			char = "│",
+		},
+		chunk = {
+			enabled = true,
+			only_current = true,
+			char = {
+				corner_top = "╭",
+				corner_bottom = "╰",
+				horizontal = "─",
+				vertical = "│",
+				arrow = "─",
+			},
+		},
+	},
+	statuscolumn = {
+		left = { "sign", "mark" }, -- priority of signs on the left (high to low)
+		right = { "fold", "git" }, -- priority of signs on the right (high to low)
+	},
+}
+
+require("snacks").setup(opts)
+
+local map = vim.keymap.set
+
+-- stylua: ignore start
+map("n", "<leader>bd", function() Snacks.bufdelete() end, { desc = "Delete Buffer" })
+map("n", "<leader>bo", function() Snacks.bufdelete.other() end, { desc = "Delete Other Buffers" })
+
+-- toggle options
+Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+Snacks.toggle.inlay_hints():map("<leader>uh")
+
+-- lazygit
+if vim.fn.executable("lazygit") == 1 then
+  map("n", "<leader>gg", function() Snacks.lazygit( { cwd = Config.root('git') }) end, { desc = "Lazygit (Root Dir)" })
+  map("n", "<leader>gG", function() Snacks.lazygit() end, { desc = "Lazygit (cwd)" })
+end
