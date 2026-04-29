17a07cce84d7098014f5b49483589b6366618429
Eric Eldredge <lettertwo@gmail.com>
2026-04-29 18:17:40 -0400

    [cvim] format mini config

--- cvim/plugin/mini.lua
+++ cvim/plugin/mini.lua
@@ -3,25 +3,25 @@ local map = vim.keymap.set
 Config.add("nvim-mini/mini.nvim")
 
 Config.once("BufReadPost", function()
-	require("mini.trailspace").setup()
-	require("mini.operators").setup()
-	require("mini.bracketed").setup()
+  require("mini.trailspace").setup()
+  require("mini.operators").setup()
+  require("mini.bracketed").setup()
 
-	require("mini.align").setup({
-		-- Module mappings. Use `''` (empty string) to disable one.
-		mappings = {
-			start = "",
-			start_with_preview = "ga",
-		},
-		modifiers = {
-			["1"] = function(steps)
-				table.insert(steps.pre_justify, require("mini.align").gen_step.filter("n == 1"))
-			end,
-		},
-	})
+  require("mini.align").setup({
+    -- Module mappings. Use `''` (empty string) to disable one.
+    mappings = {
+      start = "",
+      start_with_preview = "ga",
+    },
+    modifiers = {
+      ["1"] = function(steps)
+        table.insert(steps.pre_justify, require("mini.align").gen_step.filter("n == 1"))
+      end,
+    },
+  })
 
-	require("config.mini.ai").setup()
-	require("config.mini.surround").setup()
+  require("config.mini.ai").setup()
+  require("config.mini.surround").setup()
 end)
 
 local ext3_blocklist = { scm = true, txt = true, yml = true }
