463a4b3a5f2946af1826d916c0068861224a0c41
Eric Eldredge <lettertwo@gmail.com>
2026-05-11 14:17:53 -0400

    [cvim] inline mini.surround config

--- cvim/plugin/mini.lua
+++ cvim/plugin/mini.lua
@@ -45,7 +45,42 @@ Config.once("BufReadPost", function()
   })
 
   require("config.mini.ai").setup()
-  require("config.mini.surround").setup()
+
+  require("mini.surround").setup({
+    custom_surroundings = {
+      -- Invert the balanced bracket behaviors.
+      -- Open inserts without space, close inserts with space.
+      ["("] = { output = { left = "(", right = ")" } },
+      [")"] = { output = { left = "( ", right = " )" } },
+      ["{"] = { output = { left = "{", right = "}" } },
+      ["}"] = { output = { left = "{ ", right = " }" } },
+      ["["] = { output = { left = "[", right = "]" } },
+      ["]"] = { output = { left = "[ ", right = " ]" } },
+      ["<"] = { output = { left = "<", right = ">" } },
+      [">"] = { output = { left = "< ", right = " >" } },
+    },
+    mappings = {
+      add = "gs", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      replace = "cs", -- Replace surrounding
+
+      find = "", -- Find surrounding (to the right)
+      find_left = "", -- Find surrounding (to the left)
+      highlight = "", -- Highlight surrounding
+      suffix_last = "", -- Suffix to search with "prev" method
+      suffix_next = "", -- Suffix to search with "next" method
+      update_n_lines = "", -- Update `n_lines`
+    },
+    n_lines = 500,
+    search_method = "cover_or_next",
+    respect_selection_type = true,
+  })
+
+  -- Convenience for quickly surrounding with () or {}
+  map("x", "(", "gs(", { desc = "Add surrounding () to selection", remap = true })
+  map("x", ")", "gs)", { desc = "Add surrounding () to selection", remap = true })
+  map("x", "{", "gs{", { desc = "Add surrounding {} to selection", remap = true })
+  map("x", "}", "gs}", { desc = "Add surrounding {} to selection", remap = true })
 end)
 
 local ext3_blocklist = { scm = true, txt = true, yml = true }
