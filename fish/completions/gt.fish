9a0986d989c20ffa06903072c2ea17651b915c16
Eric Eldredge <lettertwo@gmail.com>
2026-05-14 09:45:43 -0400

    [gt] update fish completions

--- fish/completions/gt.fish
+++ fish/completions/gt.fish
@@ -13,9 +13,9 @@ function __gt_command_completions
     set -lx SHELL (type -p fish)
     set -l command (commandline -opc)
     # uncomment to include options, e.g. -q, --help
-    $command --get-yargs-completions
+    # $command --get-yargs-completions
     # uncomment to exclude options (default)
-    # $command --get-yargs-completions | string replace -r '\-.*' ''
+    $command --get-yargs-completions | string replace -r '\-.*' ''
 end
 
 # disable file completions for the entire command
@@ -24,8 +24,9 @@ complete -c gt -f
 # add completions as provided by CLI
 complete -c gt -a "(__gt_command_completions)"
 
-# commands that take branches
-complete -c gt -x -n "__fish_seen_subcommand_from checkout co bco delete dl rename rn onto --onto -o tr utr track untrack" -a "(__fish_git_local_branches)"
+# commands that take branches (local)
+complete -c gt -x -n "__fish_seen_subcommand_from checkout co bco delete dl rename rn onto --onto -o tr utr track untrack info get freeze unfreeze unlink" -a "(__fish_git_local_branches)"
 
 # gt downstack get takes remote branches
 complete -c gt -x -n "__fish_seen_subcommand_from ds dsg get g" -n "__fish_seen_subcommand_from get dsg" -a "(__fish_git_remote_branches)"
+
